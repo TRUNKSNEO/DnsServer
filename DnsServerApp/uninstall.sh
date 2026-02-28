8c14edf98584a778e81acad095d6f96c97e5afff
Shreyas Zare <shreyas@technitium.com>
2026-02-28 14:55:21 +0530

    uninstall.sh: updated script to restore network manager to default config, added support to restore original resolv.conf file along with starting up systemd-resolved. Also implemented option to remove config folder after uninstall.

--- DnsServerApp/uninstall.sh
+++ DnsServerApp/uninstall.sh
@@ -4,10 +4,12 @@ dotnetDir="/opt/dotnet"
 
 if [ -d "/etc/dns/config" ]
 then
-	dnsDir="/etc/dns"
+    dnsDir="/etc/dns"
 else
     dnsDir="/opt/technitium/dns"
 fi
+
+dnsConfig="/etc/dns"
 
 echo ""
 echo "================================="
@@ -18,26 +20,63 @@ echo "Uninstalling Technitium DNS Server..."
 
 if [ -d $dnsDir ]
 then
-	if [ "$(ps --no-headers -o comm 1 | tr -d '\n')" = "systemd" ] 
-	then
-		sudo systemctl disable dns.service >/dev/null 2>&1
-		sudo systemctl stop dns.service >/dev/null 2>&1
-		rm /etc/systemd/system/dns.service >/dev/null 2>&1
-		
-		rm /etc/resolv.conf >/dev/null 2>&1
-		echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-		echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-	fi
+    if [ "$(ps --no-headers -o comm 1 | tr -d '\n')" = "systemd" ] 
+    then
+        systemctl disable dns.service >/dev/null 2>&1
+        systemctl stop dns.service >/dev/null 2>&1
+        rm /etc/systemd/system/dns.service >/dev/null 2>&1
 
-	rm -rf $dnsDir >/dev/null 2>&1
+        rm /etc/resolv.conf >/dev/null 2>&1
 
-	if [ -d $dotnetDir ]
-	then
-		echo "Uninstalling .NET Runtime..."
-		rm /usr/bin/dotnet >/dev/null 2>&1
-		rm -rf $dotnetDir >/dev/null 2>&1
-	fi
+        if [ -f "$dnsDir/resolv.conf.bak" ] || [ -L "$dnsDir/resolv.conf.bak" ]
+        then
+            cp -a $dnsDir/resolv.conf.bak /etc/resolv.conf >/dev/null 2>&1
+        else
+            echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+            echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+        fi
+
+        if [ -f "/etc/NetworkManager/NetworkManager.conf" ]
+        then
+            currentVal=$(grep -F "dns=" /etc/NetworkManager/NetworkManager.conf)
+
+            if [ "$currentVal" = "dns=none" ]
+            then
+                sed -i "s/$currentVal/dns=default/g" /etc/NetworkManager/NetworkManager.conf >/dev/null 2>&1
+            fi
+        fi
+
+        systemctl enable systemd-resolved >/dev/null 2>&1
+        systemctl start systemd-resolved >/dev/null 2>&1
+    fi
+
+    rm -rf $dnsDir >/dev/null 2>&1
+
+    if [ -d $dotnetDir ]
+    then
+        echo "Uninstalling .NET Runtime..."
+        rm /usr/bin/dotnet >/dev/null 2>&1
+        rm -rf $dotnetDir >/dev/null 2>&1
+    fi
+
+    if [ -d "$dnsConfig" ]
+    then
+        echo ""
+        printf "Do you want to delete the config folder at '$dnsConfig' which contains all of the DNS server config files? (y/N): "
+        read -r answer
+
+        case "$answer" in
+            [Yy]* )
+                rm -rf "$dnsConfig" >/dev/null 2>&1
+                echo "The '$dnsConfig' config folder was deleted successfully."
+                ;;
+            * )
+                echo "The '$dnsConfig' config folder was not deleted and it will be reused if you install the DNS server again."
+                ;;
+        esac
+    fi
 fi
 
 echo ""
 echo "Thank you for using Technitium DNS Server!"
+echo ""
